13661c00b64908f55b3a1a7ad9b1290ed33fe827
Johan Rong <johan@DESKTOP-2HJ6MSG>
2023-12-29 18:53:12 +0100

    there were bugs now they've been swatted

--- src/command/compile.ts
+++ src/command/compile.ts
@@ -9,18 +9,20 @@
 
 import codegen from "../compiler/codegen";
 import theTime from "../util/theTime.ts";
+import * as pathjs from "path";
 
 export default async function(args: string[], startTime: number) {
     const path: string = args[1]
-    const name: string = args[1].split("/")[args.length - 1].split(".")[args.length - 2]
+    const name: string = args[1]
+        ? pathjs.basename(path, pathjs.extname(path))
+        : 'source'
     const file = Bun.file(path)
 
     if (!await file.exists()) {
         console.error("file not found")
         process.exit(1)
     }
-
-    let time = new Date().getTime()
+    
     const result: string = await codegen(await file.text())
     await Bun.write(`build/${name}.asm`, result)
     console.log(`[${theTime()}] Compilation completed in ${new Date().getTime() - startTime}ms.`)
